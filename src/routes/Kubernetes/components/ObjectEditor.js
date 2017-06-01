c9b671a9e013c1e2b97f73a543b17b262d426997
Dmitry Bashkatov <dbashkatov@gmail.com>
2017-06-01 14:20:15 +0300

    add name of edited object

--- src/routes/Kubernetes/components/ObjectEditor.js
+++ src/routes/Kubernetes/components/ObjectEditor.js
@@ -27,6 +27,7 @@ export class ObjectEditor extends React.Component {
         <div className='toolbar'>
           {object && <button onClick={detachEditor}>Detach</button>}
           <button onClick={saveObject}>Save</button>
+          {object && <span style={{float: 'right'}}>{object.metadata.namespace}/{object.metadata.name} ({object.kind})</span>}
         </div>
         <textarea className='editorArea' value={yaml} onChange={this.handleChange} />
       </div>
