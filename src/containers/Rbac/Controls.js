bf2a99c554423e7d2d9473ab4108273a8d2acd37
Denis Zhbankov <denis.zhbankov@gmail.com>
2017-06-28 13:21:22 +0300

    rbac: add legend

--- src/containers/Rbac/Controls.js
+++ src/containers/Rbac/Controls.js
@@ -54,6 +54,26 @@ class Controls extends React.Component {
             }
           </Radio.Group>
         </div>
+        <div className="rbac__legend">
+          <svg height="20">
+            <g className="node Role" transform="translate(10,10)">
+              <circle r="7" />
+              <text dx="12" dy="4">Role, ClusterRole</text>
+            </g>
+            <g className="node User" transform="translate(150,10)">
+              <circle r="7" />
+              <text dx="12" dy="4">User, Group, ServiceAccount</text>
+            </g>
+            <g className="node key" transform="translate(355,10)">
+              <circle r="7" />
+              <text dx="12" dy="4">RoleBinding, ClusterRoleBinding, key</text>
+            </g>
+            <g className="node value" transform="translate(605,10)">
+              <circle r="7" />
+              <text dx="12" dy="4">value</text>
+            </g>
+          </svg>
+        </div>
       </div>
     );
   }
