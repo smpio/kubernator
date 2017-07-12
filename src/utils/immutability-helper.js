c6fd7dc2001a4f2afe7341ecfcd9b100c3cc512b
Denis Zhbankov <denis.zhbankov@gmail.com>
2017-07-12 21:08:31 +0300

    master: use immutability-helper-extension

--- src/utils/immutability-helper.js
+++ src/utils/immutability-helper.js
@@ -1,54 +1,10 @@
-/**
- * Immutability Helper extensions.
- * @module utils/immutability-helper.
- */
-
 import update from 'immutability-helper';
+import {
+  arrayDelete,
+  arrayPushUnique,
+  objectDelete,
+} from '@uqee/immutability-helper-extensions';
 
-/**
- * Remove keys form object.
- * @param {Array|String} keys
- * @param {Object} object
- * @returns {Object} result
- */
-update.extend('$del', function(keys, object) {
-  if (!Array.isArray(keys)) keys = [keys];
-  if (object === null || typeof object !== 'object') return object;
-  else {
-    return keys.reduce(
-      (result, key) => {
-        delete result[key];
-        return result;
-      },
-      { ...object },
-    );
-  }
-});
-
-/**
- * Add values to unique array.
- * @param {Array|String} vals
- * @param {Array} array
- * @returns {Array} result
- */
-update.extend('$pushuniq', function(vals, array) {
-  if (!Array.isArray(vals)) vals = [vals];
-  if (!array || !Array.isArray(array)) return array;
-  else {
-    const res = [...array];
-    vals.forEach(val => !res.includes(val) && res.push(val));
-    return res;
-  }
-});
-
-/**
- * Remove values from array.
- * @param {Array|String} vals
- * @param {Array} array
- * @returns {Array} result
- */
-update.extend('$pop', function(vals, array) {
-  if (!Array.isArray(vals)) vals = [vals];
-  if (!array || !Array.isArray(array)) return array;
-  else return array.filter(item => !vals.includes(item));
-});
+update.extend('$pop', arrayDelete);
+update.extend('$pushuniq', arrayPushUnique);
+update.extend('$del', objectDelete);
